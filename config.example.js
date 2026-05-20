4cb945696573f74e8b8156cd375e64c3030aefbd
Németh Zoltán <zoltan.nemeth@jofogas.hu>
2026-05-20 13:13:12 +0200

    Update Supabase config

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 // Másold át "config.js" néven és töltsd ki a saját Supabase adataiddal
 window.APP_CONFIG = {
   SUPABASE_URL: 'https://YOUR-PROJECT.supabase.co',
-  SUPABASE_ANON_KEY: 'YOUR-ANON-KEY-HERE'
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_YOUR-KEY-HERE'
 };
